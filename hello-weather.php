a00ee65a53cd999d729200b0d07d21cdde2027d6
Keanu <30422582+KeanuAaron@users.noreply.github.com>
2020-05-28 22:43:20 -0500

    Update hello-weather.php

--- hello-weather.php
+++ hello-weather.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Hello Weather
- * Plugin URI: https://github.com/keanuaaron/wordpress-hello-weather-plugin
+ * Plugin URI: https://github.com/KeanuAaron/WordPress-Hello-Weather-Plugin
  * Description: This is the very first plugin I ever created. This plugin is similar to "Hello Dolly", except that it just displays the local weather of the user signed in based on their IP Address. This info isn't used anywhere or even saved. It's just a learning experience for developing wordpress plugins.
  * Version: 1.0
  * Author: Keanu Aaron
